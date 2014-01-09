5a779ad858c698aa693eeb21dfced29a344152ea
Isaac Rosenberg <irosenb7@gmail.com>
2014-01-09 14:08:55 -0500

    moved csv to top, turned site info into array

--- scrape.rb
+++ scrape.rb
@@ -30,93 +30,97 @@ email_regex = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i
 
 
 countries.each do |country|
-  country[:pages].times do |page|
-    shops = Nokogiri::HTML(open("http://shops.oscommerce.com/directory?page=#{page + 1}&country=#{country[:country]}"))
-    lis = shops.css("table + table ul li a:first-child")
-    lis.each do |li|
-      name = li.children.text 
-      iframe_url = li.attributes['href'].value
+  CSV.open("data.csv", "wb") do |csv|
+    csv << ["Name", "Link", "Type", "Owner Email", "Owner Phone", "Country", "Email", "Phone"]
+    country[:pages].times do |page|
+      shops = Nokogiri::HTML(open("http://shops.oscommerce.com/directory?page=#{page + 1}&country=#{country[:country]}"))
+      lis = shops.css("table + table ul li a:first-child")
+      lis.each do |li|
+        name = li.children.text 
+        iframe_url = li.attributes['href'].value
 
-      category = li.css("~ small").text
-      puts category
-      
-      html = Nokogiri::HTML(open(iframe_url))
-      link = html.xpath("//frame").first.attributes["<sr></sr>c"].value
-      link.slice! "live_shops_frameset_header.php?url="
-      puts link
-      
-      domain = Domainatrix.parse(link)
-      domain = "#{domain.domain}.#{domain.public_suffix}"
+        category = li.css("~ small").text
+        puts category
+        
+        html = Nokogiri::HTML(open(iframe_url))
+        link = html.xpath("//frame").first.attributes["<sr></sr>c"].value
+        link.slice! "live_shops_frameset_header.php?url="
+        puts link
+        
+        domain = Domainatrix.parse(link)
+        domain = "#{domain.domain}.#{domain.public_suffix}"
 
-      # ap contact = contact.registrant_contact.first
+        # ap contact = contact.registrant_contact.first
 
-      # email = contact.email if contact.respond_to? "email"
+        # email = contact.email if contact.respond_to? "email"
 
-      begin
-        whois = Whois.whois(domain)
-        contact = whois.registrant_contact.first
-        owner_email = contact.email
-        owner_phone = contact.phone
-      rescue 
-        owner_email = ""
-        owner_phone = ""
-        puts "no domain owner's phone/email found"
+        begin
+          whois = Whois.whois(domain)
+          contact = whois.registrant_contact.first
+          owner_email = contact.email
+          owner_phone = contact.phone
+        rescue 
+          owner_email = ""
+          owner_phone = ""
+          puts "no domain owner's phone/email found"
+        end
+
+        begin
+          html = Nokogiri::HTML(open(link))
+        rescue
+          puts "link did not work"
+          site = {:Name => name, :Link => "Possibly defunct"}
+          list << site
+          next
+        end
+
+        # binding.pry
+
+        begin
+          contact_page = html.at('a:contains("ontact")').attributes["href"].value
+          html = Nokogiri::HTML(open(contact_page))          
+          phones = html.to_s.scan(phone_regex)
+          puts phones
+          emails = html.to_s.scan(email_regex)
+          puts emails
+        rescue 
+          puts "no emails/phones found"
+          emails = []
+          phones = []
+        end
+
+        # How do we extract?
+
+        phone = phones.select { |p| Phony.plausible? p }
+        phone.uniq!
+        email = emails.empty? ? "" : emails.first 
+
+        # site = {:Name        => name, 
+        #         :Link        => link, 
+        #         :Type        => category,
+        #         :Owner_Email => owner_email,
+        #         :Owner_Phone => owner_phone,
+        #         :Country     => country[:country],
+        #         :Email       => email,
+        #         :Phone       => phone }
+        
+        site = [name, link, category, owner_email, owner_phone, country[:country], email, phone]
+
+        puts site 
+
+        csv << site
       end
-
-      begin
-        html = Nokogiri::HTML(open(link))
-      rescue
-        puts "link did not work"
-        site = {:Name => name, :Link => "Possibly defunct"}
-        list << site
-        next
-      end
-
-      # binding.pry
-
-      begin
-        contact_page = html.at('a:contains("ontact")').attributes["href"].value
-        html = Nokogiri::HTML(open(contact_page))          
-        phones = html.to_s.scan(phone_regex)
-        puts phones
-        emails = html.to_s.scan(email_regex)
-        puts emails
-      rescue 
-        puts "no emails/phones found"
-        emails = []
-        phones = []
-      end
-
-      # How do we extract?
-
-      phone = phones.select { |p| Phony.plausible? p }
-      phone.uniq!
-      email = emails.empty? ? "" : emails.first 
-
-      site = {:Name        => name, 
-              :Link        => link, 
-              :Type        => category,
-              :Owner_Email => owner_email,
-              :Owner_Phone => owner_phone,
-              :Country     => country[:country],
-              :Email       => email,
-              :Phone       => phone }
-
-      puts site 
-
-      list << site
     end
   end
 end
 
-# ["Name", "Link", "Type", "Owner Email", "Owner Phone", "Country", "Email", "Phone"]
+# 
 
 
 puts list 
 
-CSV.open("data.csv", "wb") do |csv|
-  csv << list.first.keys.collect { |k| k.gsub("_", " ")  }
-  list.each do |item|
-    csv << item.values
-  end
-end 
+#   csv << list.first.keys.collect { |k| k.gsub("_", " ")  }
+#   list.each do |item|
+#     csv << item.values
+#   end
+# end 
